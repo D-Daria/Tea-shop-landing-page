16b98ab770fc198ffb789c3bd94b3750f05a94db
Daria <Dasha_D@Darias-MacBook-Pro.local>
2020-12-05 15:31:10 +0200

    coupon_send

--- mail.php
+++ mail.php
@@ -5,14 +5,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
 
     $content = 'Отправить купон на указанный email-адрес: ' . $email;
+    
+    $coupon_send = 'Ваш купон на скидку: TEADISCOUNT';
 
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 
-    $success = mail("admin@teaberry.com", 'Ваш купон на скидку: TEADISCOUNT', $content, $headers);
+    $success = mail("admin@teaberry.com", 'Запрос на получение скидки', $content, $headers);
 
     if ($success) {
         http_response_code(200);
+        mail(" $email", 'Запрос на получение скидки', $coupon_send);
         echo "Письмо отправлено";
     } else {
         http_response_code(500);
